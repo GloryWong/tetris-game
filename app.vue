9835ac03c1d1e7a8fc2098136a2fb6450607da39
GloryWong <glorywong1001@gmail.com>
2023-04-16 02:51:06 +0800

    style: layout and kbd

--- app.vue
+++ app.vue
@@ -1,7 +1,18 @@
 <template>
   <div
-    class="flex h-screen w-screen flex-col items-center bg-slate-800 p-4 text-gray-100"
+    class="flex h-screen w-screen flex-col items-center bg-slate-800 p-5 text-gray-100"
   >
-    <GameBoard />
+    <div class="flex grow items-center justify-center">
+      <GameBoard />
+    </div>
+    <div class="text-gray-500">
+      Made with ❤️ by
+      <a
+        href="https://zhaozhao.today"
+        target="_blank"
+        class="underline"
+        >Glory Wong</a
+      >
+    </div>
   </div>
 </template>
--- components/GameBoard.vue
+++ components/GameBoard.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="m-5 flex flex-col justify-center space-y-10">
+  <div class="flex flex-col items-center justify-center space-y-10">
     <div ref="container"></div>
-    <div class="text-gray-500">Tetris Game @ 2023</div>
+    <div>
+      <div class="text-gray-500">
+        <kbd>Space</kbd> Start or Pause | <kbd>Arrow Up</kbd> Rotate |
+        <kbd>Arrow Left</kbd> Move left | <kbd>Arrow Right</kbd> Move right |
+        <kbd>Arrow Down</kbd> Soft drop
+      </div>
+    </div>
   </div>
 </template>
 
@@ -28,3 +34,18 @@ onUnmounted(() => {
   tetris?.destroy();
 });
 </script>
+
+<style>
+kbd {
+  background-color: #333;
+  border-radius: 3px;
+  border: 1px solid #444;
+  color: #789;
+  display: inline-block;
+  font-size: 0.85em;
+  font-weight: 700;
+  line-height: 1;
+  padding: 2px 4px;
+  white-space: nowrap;
+}
+</style>
